6bfbba4fabc128579c72f24cdc6b7811e0b42a42
Klittina <krisztike113@gmail.com>
2022-05-19 12:34:40 +0200

    tablaLetrehoz

--- torpedo/src/torpedo/Tabla.java
+++ torpedo/src/torpedo/Tabla.java
@@ -2,9 +2,17 @@ package torpedo;
 
 public class Tabla {
     private int sor, sorHossz, hajoHossz,hajokSzama;
+    private String [] tabla;
     
 
     public void tablaLetrehoz() {
+        
+        for (int i = 0; i < sor; i++) {
+            for (int j = 0; j < sorHossz; j++) {
+                
+            }
+            
+        }
 
     }
 
