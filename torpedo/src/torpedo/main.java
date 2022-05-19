0c56cf3398a75af2e1344873190bfeabec7b0a65
rohacsidana <rohacsi.dana@gmail.com>
2022-05-19 13:11:41 +0200

    bekérés

--- torpedo/src/torpedo/main.java
+++ torpedo/src/torpedo/main.java
@@ -1,14 +1,25 @@
 package torpedo;
 
+import java.util.Scanner;
+
 public class main {
 
+    static Tabla t1 = new Tabla(7);
+    static int tipp = 0;
+
     public static void main(String[] args) {
-        int tipp = 0;
+        
+    }
+
+    public int bekeres() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("A hajó egyik helye a " + t1.getHajoHossz() + "-ból/-ből: ");
+        int bekertSzam = sc.nextInt();
+        return bekertSzam;
 
     }
-    
-    public static void jatekKezdes(){
-        Tabla t1 = new Tabla(7);
+
+    public static void jatekKezdes() {
         t1.tablaLetrehoz();
     }
 
