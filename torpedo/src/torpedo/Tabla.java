2c8e97036a35874044d23a816640a8c388c5b64c
rohacsidana <rohacsi.dana@gmail.com>
2022-05-19 12:24:47 +0200

    commentek eltüntetése

--- torpedo/src/torpedo/Tabla.java
+++ torpedo/src/torpedo/Tabla.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package torpedo;
 
-/**
- *
- * @author Kriszti
- */
 public class Tabla {
-    
+
 }
