2c3afe66e519488900ae0e7944f268c83f6a7d69
rohacsidana <rohacsi.dana@gmail.com>
2022-05-19 12:56:36 +0200

    getter, tostring, bekeres mainben

--- torpedo/src/torpedo/Tabla.java
+++ torpedo/src/torpedo/Tabla.java
@@ -3,10 +3,10 @@ package torpedo;
 import java.util.Random;
 
 public class Tabla {
-    private int sor, sorHossz, hajoHossz,hajokSzama;
-    private String [] tabla;
-    private int [] hajoHelye;
-    
+
+    private int sor, sorHossz, hajoHossz, hajokSzama;
+    private String[] tabla;
+    private int[] hajoHelye;
 
     public Tabla(int sor, int sorHossz, int hajoHossz, int hajokSzama) {
         this.sor = sor;
@@ -22,21 +22,52 @@ public class Tabla {
     public Tabla(int sorHossz) {
         this(1, sorHossz, 3, 1);
     }
+
     public void tablaLetrehoz() {
-        int elemSzam = sor *sorHossz;
+        int elemSzam = sor * sorHossz;
         tabla = new String[elemSzam];
         for (int i = 0; i < elemSzam; i++) {
             tabla[i] = " _ ";
         }
     }
 
-    public int [] hajoHelye() {
+    public int[] hajoHelye() {
         Random rnd = new Random();
-        int elsohely= rnd.nextInt(sorHossz)-(sorHossz-hajoHossz+1);
+        int elsohely = rnd.nextInt(sorHossz) - (sorHossz - hajoHossz + 1);
         for (int i = 0; i < hajoHossz; i++) {
             hajoHelye[i] = elsohely;
             elsohely++;
         }
         return hajoHelye;
     }
+
+    public int getSor() {
+        return sor;
+    }
+
+    public int getSorHossz() {
+        return sorHossz;
+    }
+
+    public int getHajoHossz() {
+        return hajoHossz;
+    }
+
+    public int getHajokSzama() {
+        return hajokSzama;
+    }
+
+    public String[] getTabla() {
+        return tabla;
+    }
+
+    public int[] getHajoHelye() {
+        return hajoHelye;
+    }
+
+    @Override
+    public String toString() {
+        return "Tabla{" + "sor=" + sor + ", sorHossz=" + sorHossz + ", hajoHossz=" + hajoHossz + ", hajokSzama=" + hajokSzama + ", tabla=" + tabla + ", hajoHelye=" + hajoHelye + '}';
+    }
+    
 }
