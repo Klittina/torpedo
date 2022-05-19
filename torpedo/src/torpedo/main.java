87b189e02cc13f2dda4f0b99bcdb5c27dacff7a5
Klittina <krisztike113@gmail.com>
2022-05-19 12:57:30 +0200

    jatekKezdes metódus

--- torpedo/src/torpedo/main.java
+++ torpedo/src/torpedo/main.java
@@ -3,7 +3,13 @@ package torpedo;
 public class main {
 
     public static void main(String[] args) {
+        int tipp = 0;
 
+    }
+    
+    public static void jatekKezdes(){
+        Tabla t1 = new Tabla(7);
+        t1.tablaLetrehoz();
     }
 
 }
