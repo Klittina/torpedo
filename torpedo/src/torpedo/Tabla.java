c5d6db950fe9f35da052564620a07bdafeb91032
rohacsidana <rohacsi.dana@gmail.com>
2022-05-19 12:31:35 +0200

    konstruktorok létrehozása

--- torpedo/src/torpedo/Tabla.java
+++ torpedo/src/torpedo/Tabla.java
@@ -1,8 +1,23 @@
 package torpedo;
 
 public class Tabla {
-    private int sor, sorHossz, hajoHossz,hajokSzama;
-    
+
+    private int sor, sorHossz, hajoHossz, hajokSzama;
+
+    public Tabla(int sor, int sorHossz, int hajoHossz, int hajokSzama) {
+        this.sor = sor;
+        this.sorHossz = sorHossz;
+        this.hajoHossz = hajoHossz;
+        this.hajokSzama = hajokSzama;
+    }
+
+    public Tabla(int sorHossz, int hajoHossz) {
+        this(1, sorHossz, hajoHossz, 1);
+    }
+
+    public Tabla(int sorHossz) {
+        this(1, sorHossz, 3, 1);
+    }
 
     public void tablaLetrehoz() {
 
